6221ca4926290e21d281f200d3ee7094ee2bc7e6
petivip <petivip@gmail.com>
2019-11-06 13:38:16 -0500

    new change

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,2 +1,12 @@
-public class Vehicle {
+public abstract class Vehicle {
+    public void start(){
+        System.out.println("The engine have started");
+    }
+    public void move(){
+        System.out.println("The vehicle have started");
+    }
+    public void stop(){
+        System.out.println("The vehicle have stopped");
+    }
+    abstract String testMethod();
 }
